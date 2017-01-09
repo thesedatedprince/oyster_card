fa3632d47a03be4cab31584096fdf243849a55bc
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-09 12:41:21 +0000

    Test for balance and passing code

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,9 @@
-require_relative 'oystercard'
+require 'oystercard'
 
 describe Oystercard do
-
-    
+  describe "#balance" do
+    it "Checks that initial value = 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
