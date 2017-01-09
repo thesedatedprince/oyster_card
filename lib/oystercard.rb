55b5ce912f96e06d78908214a16416a35368f9a2
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-09 12:47:54 +0000

    Adding a balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,9 @@
 class Oystercard
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 1
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  it 'responds to #balance' do
+    expect(subject).to respond_to :balance
+  end
+  it 'checks that a new card has a balance' do
+    expect(subject.balance).not_to be_nil
+  end
 end
