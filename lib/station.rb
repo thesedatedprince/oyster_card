a275c0ca85da1dc0c7775a2da719d54f5887ff70
Joe Marriott <marriott90@gmail.com>
2017-01-09 16:23:22 +0000

    station class defined and station_spec created

---
+++ lib/station.rb
@@ -0,0 +1,10 @@
+class Station
+
+  attr_reader :name, :zone
+
+  def initialize(name="testname", zone="testzone")
+    @name = name
+    @zone = zone
+  end
+  
+end
---
+++ spec/station_spec.rb
@@ -0,0 +1,7 @@
+require 'station'
+
+describe Station do
+  it "Initialises a station" do
+    expect(subject).to eq subject
+  end
+end
