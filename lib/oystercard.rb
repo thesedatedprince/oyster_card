efec76c73ead8941b55e9eea8b00ca1df57b0817
Matthew Perry <thesedatedprince@gmail.com>
2017-01-09 14:53:31 +0000

    added deduct function

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,19 @@
 class Oystercard
   attr_reader :balance
 
-  LIMIT = 90
+  LIMIT = 90.0
+  FARE = 6.6
 
   def initialize
-    @balance = 1
+    @balance = 1.0
   end
 
   def top_up(add_money)
-    raise "Top up would exceed the card's balance limit of £#{LIMIT}" if @balance + add_money > LIMIT
-    @balance += add_money
+    raise "Top up would exceed the card's balance limit of £#{LIMIT}" if @balance + add_money.to_f > LIMIT
+    @balance += add_money.to_f
+  end
+
+  def deduct
+    @balance -= FARE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,7 +13,7 @@ describe Oystercard do
     expect(subject).to respond_to(:top_up).with(1).argument
   end
   it 'should add money to balance' do
-    top_amnt = 3
+    top_amnt = 3.0
     orig_amnt = subject.balance
     subject.top_up(top_amnt)
     expect(subject.balance).to eq(orig_amnt + top_amnt)
@@ -21,6 +21,17 @@ describe Oystercard do
 
   it 'should not allow the user to top up above the specified limit' do
     allow(oystercard).to receive(:balance).and_return(0)
-    expect {oystercard.top_up(100)}.to raise_error "Top up would exceed the card's balance limit of £#{Oystercard::LIMIT}"
+    expect {oystercard.top_up(100.0)}.to raise_error "Top up would exceed the card's balance limit of £#{Oystercard::LIMIT}"
+  end
+
+  it 'responds to #deduct' do
+    expect(subject).to respond_to(:deduct)
+  end
+
+  it 'should deduct money from the card' do
+    ded_amnt = 6.60
+    orig_amnt = subject.balance
+    subject.deduct
+    expect(subject.balance).to eq(orig_amnt - ded_amnt)
   end
 end
