6c17c2b52bf07c1a83e7b1130e563a200de31cde
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-09 13:17:26 +0000

    Feature of max limit added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,27 @@
 class Oystercard
 
-  attr_accessor :balance
-  DEFAULT_BALANCE = 0
+  attr_reader :balance, :limit
+  #attr_accessor :limit
 
-  def initialize(balance=DEFAULT_BALANCE)
+  DEFAULT_BALANCE = 0
+  DEFAULT_LIMIT = 90
+
+  def initialize(balance=DEFAULT_BALANCE, limit=DEFAULT_LIMIT)
     @balance = balance
+    @limit = limit
   end
 
   def top_up(deposit)
+    @deposit = deposit
+    fail "Deposit would exceed max limit of #{limit}" if hit_limit?
     @balance += deposit
   end
 
+  private
+
+  attr_reader :deposit
+
+  def hit_limit?
+    balance + deposit > limit
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,5 +11,17 @@ describe Oystercard do
     it "Tops up the balance of an Oystercard" do
       expect(subject.top_up(50)).to eq 50
     end
+
+    it "Refuse topup if limit will be exceeded" do
+      error = "Deposit would exceed max limit of #{Oystercard::DEFAULT_LIMIT}"
+      deposit = Oystercard::DEFAULT_LIMIT + 1
+      expect{subject.top_up(deposit)}.to raise_error error
+    end
+  end
+
+  describe "#limit" do
+    it "Checks card limit is £90" do
+      expect(subject.limit).to eq 90
+    end
   end
 end
