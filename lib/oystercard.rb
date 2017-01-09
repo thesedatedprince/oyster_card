820a4c6034d4f11cc672bf64bfc327e8a0a5e725
Joe Marriott <marriott90@gmail.com>
2017-01-09 12:50:31 +0000

    added unit test for top_up and defined top_up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,14 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_accessor :balance
+  DEFAULT_BALANCE = 0
 
-  def initialize
-    @balance = 0
+  def initialize(balance=DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def top_up(deposit)
+    @balance += deposit
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,10 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe "#top_up" do
+    it "Tops up the balance of an Oystercard" do
+      expect(subject.top_up(50)).to eq 50
+    end
+  end
 end
