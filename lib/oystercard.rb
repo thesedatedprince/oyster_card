2ff00196f6c30d028d5fb43ff051f2eb45e23fdb
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-09 13:30:59 +0000

    Added limit to card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,14 @@
 class Oystercard
-
   attr_reader :balance
+
+  LIMIT = 90
 
   def initialize
     @balance = 1
   end
 
   def top_up(add_money)
+    raise "Top up would exceed the card's balance limit of £#{LIMIT}" if @balance + add_money > LIMIT
     @balance += add_money
   end
-
 end
