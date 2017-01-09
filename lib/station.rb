f12164a31c7d0e1cd5d256ac240828ea6dd3ebce
Matthew Perry <thesedatedprince@gmail.com>
2017-01-09 17:40:25 +0000

    Updated station class to produce random name and zone

--- lib/station.rb
+++ lib/station.rb
@@ -1,3 +1,15 @@
 class Station
 
+
+  STATIONS = {zone_one: ["Charing Cross", "Picadilly"],
+              zone_two: ["Camden", "Baker Street"]}
+
+  attr_reader :current_zone, :current_station
+
+  def initialize
+    @current_zone = STATIONS.keys.sample
+    @current_station = STATIONS[@current_zone].sample
+  end
+
+
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,1 +1,10 @@
 require 'station'
+
+describe Station do
+it 'initializes with a zone' do
+  expect(subject.current_zone).not_to be_nil
+end
+it 'initializes with a station name' do
+  expect(subject.current_station).not_to be_nil
+end
+end
