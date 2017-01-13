8161db08a3e67766c6bd3ad73e1c5caecfd48c87
Matthew Perry <thesedatedprince@gmail.com>
2017-01-13 11:50:52 +0000

    set up start method on journey class and testing

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,8 +2,19 @@ require_relative 'journey'
 
 class JourneyLog
 
-	def initialize(journey_class = Journey.new)
+	attr_reader :journey_class, :entry_station
 
+	def initialize(journey_class = Journey.new)
+		@journey_class = journey_class
+	end
+
+	def start
+		@entry_station = journey_class.current_journey[:entry_station]
+		journey_started? = true
+	end
+
+	def journey_started?
+		false
 	end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,1 +1,29 @@
 require 'journeylog'
+
+describe JourneyLog do
+  subject(:journeylog){described_class.new(journey)}
+  let(:journey) {double :journey}
+  let(:start_station) {double :start_station}
+  #let(:current_journey)
+
+  it 'should initialize with a journey_class parameter' do
+    expect(journeylog.journey_class).to eq journey
+  end
+
+  describe '#start' do
+    it{is_expected.to respond_to(:start)}
+
+    it 'should start a new journey' do
+      subject.start
+      expect(journeylog.journey_started?).to eq true
+    end
+
+    it 'is expected to record an entry station' do
+      allow(journey).to receive(:current_journey).and_return({entry_station: start_station})
+      subject.start
+      expect(journeylog.entry_station).to eq journey.current_journey[:entry_station]
+    end
+
+  end
+
+end
